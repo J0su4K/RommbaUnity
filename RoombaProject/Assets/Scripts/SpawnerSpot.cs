e131c79e30f7a4c1f2ca59c905dc7758c740136f
Josua <148541326+J0su4K@users.noreply.github.com>
2025-02-19 22:36:01 +0100

    Update

--- RoombaProject/Assets/Scripts/SpawnerSpot.cs
+++ RoombaProject/Assets/Scripts/SpawnerSpot.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SpawnerSpot : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] GameObject spotToSpawn = null;
     void Start()
     {
         Init();
@@ -25,5 +26,10 @@ public class SpawnerSpot : MonoBehaviour
     void SpawnSpot()
     {
         //Spot spot = GameObject.Instantiate<Spot>()
+        int _randomX = Random.Range(0, 15);
+        int _randomZ = Random.Range(0, 15);
+
+        Vector3 _randomPosition = new Vector3(_randomX , 0 , _randomZ);
+        Instantiate(spotToSpawn, _randomPosition, Quaternion.identity);
     }
 }
